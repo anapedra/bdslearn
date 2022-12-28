0b6124f846cf48e56a6d9dfb79b62c1e82a01fea
Ana Lopes <anapedra.mil@gmail.com>
2022-12-28 19:54:30 -0300

    Backend project create

---
+++ backend/src/main/java/com/anapazsantanas/dslearnnbds/DslearnnbdsApplication.java
@@ -0,0 +1,13 @@
+package com.anapazsantanas.dslearnnbds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslearnnbdsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DslearnnbdsApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/anapazsantanas/dslearnnbds/DslearnnbdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anapazsantanas.dslearnnbds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslearnnbdsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
