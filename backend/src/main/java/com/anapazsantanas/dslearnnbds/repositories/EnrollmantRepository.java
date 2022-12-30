481c0eee4d6ce632d30f6a957a40bae7762a196a
Ana Lopes <anapedra.mil@gmail.com>
2022-12-30 09:42:40 -0300

    Fix: EnrollmentRepository

--- backend/src/main/java/com/anapazsantanas/dslearnnbds/repositories/EnrollmantRepository.java
+++ backend/src/main/java/com/anapazsantanas/dslearnnbds/repositories/EnrollmantRepository.java
@@ -1,9 +1,10 @@
 package com.anapazsantanas.dslearnnbds.repositories;
 
 import com.anapazsantanas.dslearnnbds.entities.Enrollment;
+import com.anapazsantanas.dslearnnbds.entities.pk.EnrollmantPK;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EnrollmantRepository extends JpaRepository<Enrollment,Long> {
+public interface EnrollmantRepository extends JpaRepository<Enrollment,EnrollmantPK> {
 }
